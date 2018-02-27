376a7986b3a340828d6944337531aa42e77eec3d
vritant24 <vritoku@gmail.com>
2018-02-27 17:08:00 -0500

    refactored code

--- sys_exec.py
+++ sys_exec.py
@@ -10,8 +10,7 @@ def exec_command_line(command):
 
 # takes an array and creates string of elements separated with spaces
 def gen_command(arr):
-    if(arr == []): return ""
-    return reduce((lambda acc, el: acc + " " + el), arr)
+    return "" if (arr == []) else reduce((lambda acc, el: acc + " " + el), arr)
 
 # Takes a c file name, an array of options for GCC, and an output file name
 # Runs the GCC compiler on the file
@@ -32,3 +31,4 @@ def run_output_file(output_file):
 # EXAMPLE: 
 compile_c_file("./test.c", [], "output")
 run_output_file("output")
+exec_command_line("rm output")
