64da9c57952fccc91c7a7aeb406ea087843617c9
James Decker <decker31@purdue.edu>
2018-03-02 15:01:44 -0500

    Fixing ast.py

--- ast.py
+++ ast.py
@@ -8,166 +8,32 @@ import inspect
 import builtins
 import virtualized
 
-def parametrized(dec):
-    def layer(*args, **kwargs):
-        def repl(f):
-            return dec(f, *args, **kwargs)
-        return repl
-    return layer
-
-################################################
-
-class IR(object): pass
-
-class IRConst(IR):
-    def __init__(self, v):
-        self.v = v
-
-class IRInt(IR):
-    def __init__(self, n):
-        self.n = n
-
-class IRIntAdd(IR):
-    def __init__(self, lhs, rhs):
-        self.lhs = lhs
-        self.rhs = rhs
-
-class IRIntMul(IR):
-    def __init__(self, lhs, rhs):
-        self.lhs = lhs
-        self.rhs = rhs
-
-class IRIf(IR):
-    def __init__(self, cnd, thn, els): NotImplemented
-
-class IRRet(IR):
-    def __init__(self, val): NotImplemented
-
-################################################
-
-class PyGenIRConst(object):
-    def gen(self, irconst): return str(irconst.v)
-
-class PyGenIRInt(object):
-    def gen(self, irint): return str(irint.n)
-
-class PyGenIRIntAdd(object):
-    def gen(self, iradd):
-        lhscode = PyCodeGen(iradd.lhs).gen()
-        rhscode = PyCodeGen(iradd.rhs).gen()
-        return "{0} + {1}".format(lhscode, rhscode)
-
-class PyGenIRIntMul(object):
-    def gen(self, irmul):
-        lhscode = PyCodeGen(irmul.lhs).gen()
-        rhscode = PyCodeGen(irmul.rhs).gen()
-        return "{0} * {1}".format(lhscode, rhscode)
-
-class CodeGen(object): pass
-
-class PyCodeGen(CodeGen):
-    def __init__(self, ir):
-        self.ir = ir
-    def gen(self):
-        clsName = "PyGen{0}".format(type(self.ir).__name__)
-        modl = sys.modules[__name__]
-        Cls = getattr(modl, clsName)
-        return Cls().gen(self.ir)
-
-class CCodeGen(CodeGen): pass
-
-################################################
-
-class RepTyp(object): pass
-
-class RepInt(RepTyp):
-    def __init__(self, n):
-        self.n = n
-    def __IR__(self):
-        if isinstance(self.n, int): return IRConst(self.n)
-        else: return IRInt(self.n)
-    def __add__(self, m):
-        if isinstance(m, RepTyp): m = m.__IR__()
-        return IRIntAdd(self.__IR__(), m)
-    def __mul__(self, m):
-        if isinstance(m, RepTyp): m = m.__IR__()
-        return IRIntMul(self.__IR__(), m)
-
-################################################
-
-class RepFunc():
-    def __init__(self, func, *args, **kwargs):
-        self.func = func
-        #TODO
-
-class CompiledCode:
-    def __init__(self):
-        pass
-
 class AstVisitor(ast.NodeVisitor):
     def __init__(self):
         super()
 
     def visit_If(self, node):
-        # TODO: Virtualization of `if`
-        # If the condition part relies on a staged value, then it should be virtualized.
-        # print(ast.dump(node))
-
-        # if node is of the form (test, body, orelse)
-
-        # iter_fields lets me iterate through the contents of the if node
-        # gives the child as a tuple of the form (child-type, object)
-        # print(ast.dump(node))
-        # print(ast.dump(node.test))
-        # print("iffff")
-        cond_node = node.test;
-        # check for BoolOp and then Compare
-
-        x = ast.Call(func=ast.Name('__if', ast.Load()), args=[node.test, node.body, node.orelse], keywords=[])
-        # print("defined x")
-        # print(ast.dump(x))
-
         self.generic_visit(node)
-        return node
 
     def visit_While(self, node):
-        # TODO: Virtualization of `while`
-        # print("while")
         self.generic_visit(node)
-        return node
 
     def visit_FunctionDef(self, node):
         print("(def {0}")
 
-        #visit all child nodes
+        #visit all child nodes using self.generic_visit(child nodes...)
 
         print(")")
 
         # Drop the decorator
         self.generic_visit(node)
         node.decorator_list = []
-        return node
 
     def visit_Return(self, node):
         self.generic_visit(node)
-        # TODO: just a poor hack to make power work
-        if ast.dump(node.value) == ast.dump(ast.Num(1)):
-            ret = ast.copy_location(ast.Return(value=ast.Call(func=ast.Name(id='RepInt', ctx=ast.Load()),
-                                                               args=[ast.Num(1)],
-                                                               keywords=[])),
-                                     node)
-
-            return ret
-        return node
 
     def visit_Name(self, node):
         self.generic_visit(node)
-        if node.id in self.reps:
-            return ast.copy_location(ast.Call(func=ast.Name(id=self.reps[node.id].__name__, ctx=ast.Load()),
-                                              args=[ast.Str(s=node.id)],
-                                              keywords=[]),
-                                    node)
-        return node
 
 def ast(obj):
     """
