72edd7795924d85bef6e7c78058247cbef359827
Tiark Rompf <tiark.rompf@gmail.com>
2018-03-04 10:14:28 -0500

    python3 syntax in test.py, as requested by .travis.yml

--- test.py
+++ test.py
@@ -19,57 +19,57 @@ def buildspaces(tab):
 class RepInt(object):
   def __init__(self, tab=0):
     self.val = freshName()
-    print "{}int {};".format(buildspaces(tab), self.val)
+    print("{}int {};".format(buildspaces(tab), self.val))
 
   def add(self, y, tab=0):
     if type(y) is RepString:
       return y.add2(self, tab)
 
     res = RepInt(tab)
-    print "{}{} = {} + {};".format(buildspaces(tab), res.val, self.val, y.val)
+    print("{}{} = {} + {};".format(buildspaces(tab), res.val, self.val, y.val))
     return res
 
   def read(self, tab=0):
     # res = RepInt()
-    print "{}fin >> {};".format(buildspaces(tab), self.val)
+    print("{}fin >> {};".format(buildspaces(tab), self.val))
     return self
 
   def print1(self, tab=0):
-    print "{}std::cout << {};".format(buildspaces(tab), self.val)
+    print("{}std::cout << {};".format(buildspaces(tab), self.val))
     return
 
 class RepString(object):
   def __init__(self, tab=0):
     self.val = freshName()
-    print "{}string {};".format(buildspaces(tab), self.val)
+    print("{}string {};".format(buildspaces(tab), self.val))
 
   def add(self, y, tab=0):
     res = RepString()
-    print "{}{} = {} + {};".format(buildspaces(tab), res.val, self.val, y.val)
+    print("{}{} = {} + {};".format(buildspaces(tab), res.val, self.val, y.val))
     return res
 
   def add2(self, y, tab=0):
     res = RepString()
-    print "{}{} = {} + {};".format(buildspaces(tab), res.val, y.val, self.val)
+    print("{}{} = {} + {};".format(buildspaces(tab), res.val, y.val, self.val))
     return res
 
   def read(self, tab=0):
     # res = RepString()
-    print "{}fin >> {}".format(buildspaces(tab), res.val)
+    print("{}fin >> {}".format(buildspaces(tab), res.val))
     return self
 
   def print1(self, tab=0):
-    print "{}std::cout << {};".format(buildspaces(tab), self.val)
+    print("{}std::cout << {};".format(buildspaces(tab), self.val))
     return
 
 def staged_read(tab=0):
   res = RepInt(tab)
-  print "{}fin >> {};".format(buildspaces(tab), res.val)
+  print("{}fin >> {};".format(buildspaces(tab), res.val))
   return res
 
 def staged_read_str(tab=0):
   res = RepString(tab)
-  print "{}fin >> {};".format(buildspaces(tab), res.val)
+  print("{}fin >> {};".format(buildspaces(tab), res.val))
   return res
 
 class RepIntD(object):
@@ -102,12 +102,12 @@ class RepStringD(object):
 class RepAny(object):
   def __init__(self, *args, **kwargs):
     # just implementing for this example
-    print "-------RepAny-------"
+    print("-------RepAny-------")
     for arg in args:
       self.arg = arg
-      print arg
+      print(arg)
       # print type(arg)
-    print "-------xxxxxx-------" 
+    print("-------xxxxxx-------" )
     return
 
   def __call__(self, fn):
@@ -143,18 +143,18 @@ def readSchema():
   return schema
 
 def startSnippet():
-  print "#include<fstream>"
-  print "#include<iostream>"
-  print "#include<string>"
-  print ""
-  print "int main() {"
-  print "  fstream fin;"
-  print "  fin.open(\"data.csv\");"
+  print("#include<fstream>")
+  print("#include<iostream>")
+  print("#include<string>")
+  print("")
+  print("int main() {")
+  print("  fstream fin;")
+  print("  fin.open(\"data.csv\");")
   return
 def endSnippet():
-  print "  fin.close();"
-  print "}"
-  print ""
+  print("  fin.close();")
+  print("}")
+  print("")
 
 startSnippet()
 schema = readSchema()
