a4b58b9f72eb614f278cf579a7bf958c48b1733d
Gregory Essertel <gregory.essertel@gmail.com>
2018-03-28 10:22:44 -0400

    Missing .h file

---
+++ gen/lantern.h
@@ -0,0 +1,18 @@
+#include <sys/time.h>
+
+int HEAP_SIZE = 1073741826; // 1048576;  //2147483652; //536870912; // 268435456; //2097152;
+void *mallocBase = malloc(HEAP_SIZE);
+void *mallocAddr = mallocBase;
+void *waterMark  = mallocBase;
+void* myMalloc(size_t bytes) {
+  void* res = mallocAddr;
+  mallocAddr += bytes;
+  return res;
+}
+
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1) {
+  long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+  result->tv_sec = diff / 1000000;
+  result->tv_usec = diff % 1000000;
+  return (diff<0);
+}
