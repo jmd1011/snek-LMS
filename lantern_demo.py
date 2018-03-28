01492c67cc1af5956cac7f754fff5e96d5f9ae3f
James Decker <decker31@purdue.edu>
2018-03-28 17:22:02 -0400

    Fixing lantern_demo

--- lantern_demo.py
+++ lantern_demo.py
@@ -1,13 +1,5 @@
 from __future__ import print_function
-import argparse
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.autograd import Variable
-import time
-from pylms import lms, stage
+from pylms import lms, stage, stageTensor
 from pylms.rep import Rep
 
 # if __name__ == '__main__':
@@ -197,7 +189,7 @@ def run(train_loader):
 
 print(run.src)
 
-@stage
+@stageTensor
 def runX(x):
     return run(x)
 
