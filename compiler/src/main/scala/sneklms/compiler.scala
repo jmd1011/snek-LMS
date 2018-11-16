2f373f4f8acf3276973b0bc2ae65e8ba0ba866a9
Fei Wang <wang603@purdue.edu>
2018-11-16 01:06:56 -0500

    fix snekDriveC

--- compiler/src/main/scala/sneklms/compiler.scala
+++ compiler/src/main/scala/sneklms/compiler.scala
@@ -1150,67 +1150,93 @@ abstract class SnekDslDriverC[A:Manifest,B:Manifest](ddir: String, mmoduleName: 
       val sA = remap(manifest[A])
 
       withStream(out) {
-        stream.println(s"""/**************************/
-#include <stdio.h>
-#include <iostream>
-#include <stdlib.h>
-#include <stdint.h>
-#include <math.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <errno.h>
-#include <err.h>
-#include <sys/time.h>
-#include <time.h>
-#include <functional>
-#include <memory>
-#include <random>
-#include "lantern.h"
-#include "$moduleName.h"
-"""
-+
-"""using namespace std;
-#ifndef MAP_FILE
-#define MAP_FILE MAP_SHARED
-#endif
-int printll(char* s) {
-  while (*s != '\n' && *s != ',' && *s != '\t') {
-    putchar(*s++);
-  }
-  return 0;
-}
-long hash(char *str0, int len)
-{
-  unsigned char* str = (unsigned char*)str0;
-  unsigned long hash = 5381;
-  int c;
-
-  while ((c = *str++) && len--)
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-  return hash;
-}
-
-int entrypoint(int);
-
-std::random_device rd{};
-std::mt19937 gen{rd()};
-std::normal_distribution<> d{0,1};
-
-int main(int argc, char *argv[])
-{
-
-  if (argc != 2) {
-    printf("usage: query <filename>\n");
-    return 0;
-  }
-  return entrypoint(atoi(argv[1]));
-}
-
-      """.stripMargin)
+        stream.println(raw"""
+        |#include <stdio.h>
+        |#include <iostream>
+        |#include <stdlib.h>
+        |#include <stdint.h>
+        |#include <math.h>
+        |#include <unistd.h>
+        |#include <sys/types.h>
+        |#include <sys/stat.h>
+        |#include <fcntl.h>
+        |#include <sys/mman.h>
+        |#include <errno.h>
+        |#include <err.h>
+        |#include <sys/time.h>
+        |#include <time.h>
+        |#include <functional>
+        |#include <memory>
+        |#include <random>
+        |#include "lantern.h"
+        |#include "$moduleName.h"
+        |using namespace std;
+        |#ifndef MAP_FILE
+        |#define MAP_FILE MAP_SHARED
+        |#endif
+        |
+        |int fsize(int fd) {
+        |  struct stat stat;
+        |  int res = fstat(fd, &stat);
+        |  return stat.st_size;
+        |}
+        |
+        |int printll(char *s) {
+        |  while (*s != '\n' && *s != ',' && *s != '\t') {
+        |    putchar(*s++);
+        |  }
+        |  return 0;
+        |}
+        |
+        |long hash(char *str0, int len) {
+        |  unsigned char *str = (unsigned char *)str0;
+        |  unsigned long hash = 5381;
+        |  int c;
+        |
+        |  while ((c = *str++) && len--)
+        |    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        |
+        |  return hash;
+        |}
+        |
+        |long HEAP_SIZE_CPU = 1073741826; // 1048576; // 536870912; // 268435456; // 2097152; 1610612739; // 4294967304; //
+        |void *mallocBase = calloc(HEAP_SIZE_CPU, 1);
+        |void *mallocAddr = mallocBase;
+        |void *waterMark = mallocBase;
+        |void *myMalloc(size_t bytes) {
+        |  void *res = mallocAddr;
+        |  mallocAddr = (void *)((char *)mallocAddr + bytes);
+        |  if ((long)mallocAddr >= (long)mallocBase + HEAP_SIZE_CPU)
+        |    fprintf(stderr, "CPU memory breached limit of HEAP_SIZE_CPU\n");
+        |  return res;
+        |}
+        |
+        |long HEAP_SIZE = 4294967304; // this is for GPU
+        |
+        |int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1) {
+        |  long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+        |  result->tv_sec = diff / 1000000;
+        |  result->tv_usec = diff % 1000000;
+        |  return (diff < 0);
+        |}
+        |
+        |$templateRawCode
+        |
+        |void Snippet(char *);
+        |
+        |std::random_device rd{};
+        |std::mt19937 gen{rd()};
+        |std::normal_distribution<> d{0, 0.01};
+        |
+        |int main(int argc, char *argv[]) {
+        |  if (argc != 2) {
+        |    printf("usage: query <filename>\n");
+        |    return 0;
+        |  }
+        |  Snippet(argv[1]);
+        |  return 0;
+        |}
+        |""".stripMargin)
 
         emitFunctions(dir, moduleName)
 
